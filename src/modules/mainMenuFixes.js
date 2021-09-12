2a985a225b6c3fa74889571190922530abff0629
plungingChode <szigeti.peter23@gmail.com>
2021-09-12 19:05:44 +0200

    Simplify mainMenuFixes module
    Avoid inserting extra elements where possible

--- src/modules/mainMenuFixes.js
+++ src/modules/mainMenuFixes.js
@@ -1,82 +1,72 @@
 const $ = window.jQuery;
 const utils = require("../utils");
 
-// Fix opening in new tab and add shortcuts
-function fixMenu() {
-  const color = $("#lbtnQuit").css("color");
+// Add shortcuts to main menu
 
-  $("#mb1_Tanulmanyok")
-    .attr("targeturl", "main.aspx?ctrl=0206&ismenuclick=true")
-    .attr("hoverid", "#mb1_Tanulmanyok_Leckekonyv");
-  $("#mb1_Targyak")
-    .attr("targeturl", "main.aspx?ctrl=0303&ismenuclick=true")
-    .attr("hoverid", "#mb1_Targyak_Targyfelvetel");
-  $("#mb1_Vizsgak")
-    .attr("targeturl", "main.aspx?ctrl=0401&ismenuclick=true")
-    .attr("hoverid", "#mb1_Vizsgak_Vizsgajelentkezes");
+/**
+ * Designate the element with `shortcutId` as a shortcut for `targetId`.
+ * 
+ * @param {string} shortcutId
+ * @param {string} targetId 
+ */
+function createShortcut(shortcutId, targetId) {
+  const shortcut = $(shortcutId);
+  const target = $(targetId);
 
-  const orarend = $(`
-    <li aria-haspopup="false" tabindex="0" role="menuitem"
-      class="menu-parent has-target"
-      id="mb1_Orarend"
-      targeturl="main.aspx?ctrl=0203&amp;ismenuclick=true">
-      Órarend
+  shortcut.on("click", () => (window.location.search = target.attr("targeturl")));
+
+  // TODO maybe this works with toggleClass() ?
+  shortcut.hover(
+    () => target.addClass("menu-hover"),
+    () => target.removeClass("menu-hover")
+  );
+}
+function addShortcuts() {
+  createShortcut("#mb1_Tanulmanyok", "#mb1_Tanulmanyok_Leckekonyv");
+  createShortcut("#mb1_Targyak", "#mb1_Targyak_Targyfelvetel");
+  createShortcut("#mb1_Vizsgak", "#mb1_Vizsgak_Vizsgajelentkezes");
+}
+
+/**
+ * Move the timetable menu item from the studies submenu to the main menu.
+ */
+function moveTimetable() {
+  const timetable = $("#mb1_Tanulmanyok_Órarend");
+  $("#mb1_Targyak").before(timetable);
+
+  timetable.on("click", () => (window.location.search = timetable.attr("targeturl")));
+}
+
+/**
+ * Add a link to Meet Street / Neptun to the main menu.
+ */
+function addMeetStreetToggle() {
+  const isNeptun = $("#upChooser_chooser_neptun").hasClass("NeptunChooserSelected");
+  let menuItem = {target: null, caption: null};
+  
+  if (isNeptun) {
+    menuItem = {target: "btnKollab", caption: "Meet Street"}
+  } else {
+    menuItem = {target: "btnNeptun", caption: "Neptun"}
+  }
+
+  // Add item to main menu
+  $("#mb1").append($(`
+    <li class="menu-item">
+      <a href="javascript:__doPostBack('upChooser$${menuItem.target}','')">
+        ${menuItem.caption}
+      </a>
     </li>
-  `);
-  $("#mb1_Targyak").before(orarend);
-  $("#mb1_Tanulmanyok_Órarend").remove();
+  `));
+}
 
-  if (!$("#upChooser_chooser_kollab").hasClass("KollabChooserSelected")) {
-    $(`
-      <li aria-haspopup="false" tabindex="0" role="menuitem"
-        class="menu-parent has-target"
-        id="mb1_MeetStreet"
-        targeturl="javascript:__doPostBack('upChooser$btnKollab','')">
-        Meet Street
-      </li>
-    `).appendTo("#mb1");
-  }
-  if (!$("#upChooser_chooser_neptun").hasClass("NeptunChooserSelected")) {
-    $(`
-      <li aria-haspopup="false" tabindex="0" role="menuitem"
-        class="menu-parent has-target"
-        id="mb1_TanulmanyiRendszer"
-        targeturl="javascript:__doPostBack('upChooser$btnNeptun','')">
-        Neptun
-      </li>
-    `).appendTo("#mb1");
-  }
-
-  $("#mb1 li[targeturl]")
-    .css("position", "relative")
-    .each(function () {
-      $(this).addClass("has-target");
-      const target = $(this).attr("targeturl");
-      const a = $(`
-        <a href="${target}" style="display: block; position: absolute; left: 0; top: 0; width: 100%; height: 100%"></a>
-      `);
-      a.click(function (e) {
-        $("ul.menu").css("visibility", "hidden");
-        e.stopPropagation();
-      });
-      const hoverid = $(this).attr("hoverid");
-      if (hoverid) {
-        a.hover(
-          () => {
-            $(hoverid).addClass("menu-hover");
-          },
-          () => {
-            $(hoverid).removeClass("menu-hover");
-          }
-        );
-      }
-      $(this).append(a);
-    });
+function initialize() {
+  addShortcuts();
+  moveTimetable();
+  addMeetStreetToggle();
 }
 
 module.exports = {
   shouldActivate: () => utils.isLoggedIn(),
-  initialize: () => {
-    fixMenu();
-  },
+  initialize,
 };
